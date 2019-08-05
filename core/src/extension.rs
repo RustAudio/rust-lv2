f8e6fc528309c550b6a9c9411d9fb2b4261d2a82
Janonard <janonard@protonmail.com>
2019-08-05 10:51:11 +0200

    Updating the documentation of the extension system

--- core/src/extension.rs
+++ core/src/extension.rs
@@ -3,28 +3,21 @@ use crate::plugin::Plugin;
 use crate::UriBound;
 use std::any::Any;
 
-/// A trait for marking a type as an LV2 Plugin extension.
+/// A trait for marking a type as an LV2 plugin extension.
 ///
 /// # Unsafety
 ///
-/// Failing to meet any of these requirements will lead to undefined behavior when the host will
-/// try to load the extension for any given plugin.
+/// This trait is unsafe since the [`RAW_DATA`](#associatedconstant.RAW_DATA) constant has special requirements that can not be enforced with Rust's type system. Not complying to these requirements may induce undefined behaviour.
 ///
-pub unsafe trait Extension<P: Plugin>: UriBound {
-    /// The raw data structure defined by the extension, as returned by the plugin's `extension_data()` method.
-    ///
-    /// It can be set to any static value, it is up to the implementer of the
-    /// extension to set it correctly:
-    ///
-    /// * The struct being pointed to must be `#[repr(C)]` and have exactly the same fields as the one defined by the LV2 Extension specification.
-    /// * The struct being pointed to must be correctly initialized, as defined by the LV2 Extension specification;
-    /// * The URI associated to this extension must be exactly the same as the one defined in the LV2 Extension specification, and must also be the one tied to the struct being pointed to by `RAW_DATA`.
-    const RAW_DATA: &'static (dyn Any + 'static);
-}
-
-/// Generate a method body for a plugin's `extension_data` method.
+/// # Usage
 ///
-/// In most cases, you don't need to implement [`extension_data`](plugin/trait.Plugin.html#method.extension_data) yourself, since most dynamic extension objects implement [`Extension`](extension/trait.Extension.html). An example:
+/// This trait itself is only important to extension developers/implementers. However, it induces some interesting details for all users:
+///
+/// First of all, extension traits must be object-safe, since at one point, extended struct references must be turned into trait objects. This means, for example, that trait method may not return `Self` or must always take `self` as their first argument. You can read more about object-safety [in the book](https://doc.rust-lang.org/book/ch17-02-trait-objects.html?highlight=trait,object#object-safety-is-required-for-trait-objects).
+///
+/// To plugin developers, handling extensions is relatively easy: You simply implement the extension trait for your plugin and generate a body for the [`extension_data`](../plugin/trait.Plugin.html#method.extension_data) method using the [`match_extensions`](../macro.match_extensions.html) macro.
+///
+///  An example on how to put it all together:
 ///
 ///     use lv2_core::plugin::{Plugin, PluginInfo, FeatureContainer};
 ///     use lv2_core::extension::Extension;
@@ -108,6 +101,22 @@ pub unsafe trait Extension<P: Plugin>: UriBound {
 ///         .downcast_ref()
 ///         .unwrap();
 ///     unsafe { assert_eq!((interface.foo)(&my_plugin), 17.0) }
+///
+pub unsafe trait Extension<P: Plugin>: UriBound {
+    /// The raw data structure defined by the extension, as returned by the plugin's `extension_data()` method.
+    ///
+    /// It can be set to any static value, it is up to the implementer of the
+    /// extension to set it correctly:
+    ///
+    /// * The struct being pointed to must be `#[repr(C)]` and have exactly the same fields as the one defined by the LV2 Extension specification.
+    /// * The struct being pointed to must be correctly initialized, as defined by the LV2 Extension specification;
+    /// * The URI associated to this extension must be exactly the same as the one defined in the LV2 Extension specification, and must also be the one tied to the struct being pointed to by `RAW_DATA`.
+    const RAW_DATA: &'static (dyn Any + 'static);
+}
+
+/// Generate a method body for a plugin's `extension_data` method.
+///
+/// This macros is designed to be used inside the [`Plugin::extension_data`](plugin/trait.Plugin.html#method.extension_data) method. It takes the expression of the uri as the first argument, followed by a list of extensions. Please take a look at the [`Extension` trait](extension/trait.Extension.html) for a usage example.
 #[macro_export]
 macro_rules! match_extensions {
     ($uri:expr, $($extension:ty),*) => {
