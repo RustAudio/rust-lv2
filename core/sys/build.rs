e7bfbff85d2b7f3bd4882fd02d2541d43fabef91
Janonard <janonard@protonmail.com>
2019-08-06 17:54:48 +0200

    Generating core sys bindings when compiling

---
+++ core/sys/build.rs
@@ -0,0 +1,16 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    let bindings = bindgen::Builder::default()
+        .header("src/lv2.h")
+        .generate()
+        .expect("Unable to generate bindings");
+
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
