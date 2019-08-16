dfc433aa86f579bc4ba491440f7d185985dbbacf
Janonard <jan.opdenhoevel@protonmail.com>
2019-08-16 20:16:01 +0200

    Simplifying the URIDCache derive macro even further

--- urid/derive/src/urid_cache_derive.rs
+++ urid/derive/src/urid_cache_derive.rs
@@ -1,52 +1,31 @@
 use proc_macro::TokenStream;
 use syn::DeriveInput;
-use syn::{parse_macro_input, Data, DataStruct, Ident};
-
-/// Representation of the cache struct we implement `URIDCache` for.
-struct CacheStruct<'a> {
-    struct_name: &'a Ident,
-    fields: Vec<&'a Ident>,
-}
-
-impl<'a> CacheStruct<'a> {
-    /// Construct a cache representation from the derive input.
-    fn from_derive_input(input: &'a DeriveInput) -> Self {
-        let struct_name = &input.ident;
-        let fields = match &input.data {
-            Data::Struct(DataStruct { fields, .. }) => fields
-                .iter()
-                .map(|field| field.ident.as_ref().unwrap())
-                .collect(),
-            _ => panic!("Only structs can implement `FeatureCollection`"),
-        };
-        Self {
-            struct_name,
-            fields,
-        }
-    }
-
-    /// Construct the implementation fo `URIDCache` for our cache.
-    fn make_implementation(&self) -> TokenStream {
-        let struct_name = self.struct_name;
-        let initializations = self
-            .fields
-            .iter()
-            .map(|ident| quote! {#ident: map.populate_cache()?,});
-        let implementation = quote! {
-            impl ::lv2_urid::URIDCache for #struct_name {
-                fn from_map(map: &::lv2_urid::Map) -> Option<Self> {
-                    Some(Self {
-                        #(#initializations)*
-                    })
-                }
-            }
-        };
-        implementation.into()
-    }
-}
+use syn::{parse_macro_input, Data, DataStruct};
 
 pub fn urid_cache_derive_impl(input: TokenStream) -> TokenStream {
     let input: DeriveInput = parse_macro_input!(input);
-    let cache_struct = CacheStruct::from_derive_input(&input);
-    cache_struct.make_implementation()
+
+    let struct_name = input.ident;
+
+    let fields = match input.data {
+        Data::Struct(DataStruct { fields, .. }) => fields,
+        _ => panic!("Only structs can implement `URIDCache`"),
+    };
+
+    let field_inits = fields
+        .iter()
+        .map(|field| field.ident.as_ref().unwrap())
+        .map(|ident| quote! {#ident: map.populate_cache()?,});
+
+    let implementation = quote! {
+        impl ::lv2_urid::URIDCache for #struct_name {
+            fn from_map(map: &::lv2_urid::Map) -> Option<Self> {
+                Some(Self {
+                    #(#field_inits)*
+                })
+            }
+        }
+    };
+
+    implementation.into()
 }
