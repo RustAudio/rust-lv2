cc3f39941f5572698139fb54ba17f371419face4
Jan-Oliver Janonard opdenhoevel <jan.opdenhoevel@protonmail.com>
2019-09-22 10:19:07 +0200

    Adding UriBound to the Core prelude

--- core/src/prelude.rs
+++ core/src/prelude.rs
@@ -2,4 +2,4 @@
 pub use crate::feature::{FeatureCollection, FeatureContainer, MissingFeatureError};
 pub use crate::plugin::{lv2_descriptors, Plugin, PluginInfo, PortContainer};
 pub use crate::port::*;
-pub use crate::Uri;
+pub use crate::{Uri, UriBound};
