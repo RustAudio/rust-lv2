1bc81a7de48f4b97e1cefe77145b4cfdb3e5931a
Jan-Oliver Opdenhövel <janonard@protonmail.com>
2019-10-05 12:23:26 +0200

    Delete environment.nix

--- environment.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {}}:
-pkgs.mkShell {
-    librarySystemDepends = with pkgs; [ clang ];
-    buildInputs = with pkgs; [
-        rustup
-        llvmPackages.clang.cc.lib
-    ];
-    LIBCLANG_PATH = ''${pkgs.llvmPackages.clang.cc.lib}/lib'';
-}
