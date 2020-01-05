eb56c8cdb407ccd0d55dd593f2b809cb345d66c8
Jan-Oliver 'Janonard' Opdenhövel <jan.opdenhoevel@protonmail.com>
2020-01-05 12:09:45 +0100

    Creating a safe and dynamic atom space that works and doesn't break the system

--- atom/src/space.rs
+++ atom/src/space.rs
@@ -8,10 +8,8 @@
 //!
 //! The second case is sound since a) the data is contained in a slice and therefore is accessible, b) generic type parameter bounds assure that the type is plain-old-data and c) 64-bit padding is assured.
 use std::cell::Cell;
-use std::iter::FromIterator;
 use std::marker::Unpin;
 use std::mem::{size_of, size_of_val};
-use std::pin::Pin;
 use urid::URID;
 
 /// Specialized smart pointer to retrieve struct instances from a slice of memory.
@@ -240,51 +238,120 @@ impl<'a> MutSpace<'a> for RootMutSpace<'a> {
     }
 }
 
-/// A mutable space that owns its data and dynamically allocates more memory when needed.
-pub struct BufferedMutSpace {
-    blocks: Vec<Pin<Box<[u8]>>>,
-    allocated_bytes: usize,
+/// Linked list element for dynamic atom writing.
+/// 
+/// This struct works in conjunction with [`SpaceHead`](struct.SpaceHead.html) to provide a way to write atoms to dynamically allocated memory.
+pub struct SpaceElement {
+    next: Option<(Box<Self>, Box<[u8]>)>,
 }
 
-impl Into<Vec<u8>> for BufferedMutSpace {
-    fn into(self) -> Vec<u8> {
-        let mut vector = Vec::new();
-        vector.extend(self.blocks.iter().map(|chunk| chunk.iter()).flatten());
-        assert_eq!(vector.len(), self.allocated_bytes);
-        vector
+impl Default for SpaceElement {
+    fn default() -> Self {
+        Self { next: None }
     }
 }
 
-impl Default for BufferedMutSpace {
-    fn default() -> Self {
+impl SpaceElement {
+    /// Append an element to the list.
+    /// 
+    /// If this is the last element of the list, allocate a slice of the required length and append a new element to the list. If not, do nothing and return `None`.
+    pub fn allocate(&mut self, size: usize) -> Option<(&mut Self, &mut [u8])> {
+        if self.next.is_some() {
+            return None;
+        }
+
+        let new_data = vec![0u8; size].into_boxed_slice().into();
+        let new_element = Box::new(Self::default());
+        self.next = Some((new_element, new_data));
+        self.next
+            .as_mut()
+            .map(|(new_element, new_data): &mut (Box<Self>, Box<[u8]>)| {
+                (new_element.as_mut(), new_data.as_mut())
+            })
+    }
+
+    /// Create a vector containing the data from all elements following this one.
+    pub fn to_vec(&self) -> Vec<u8> {
+        self.iter().map(|slice| slice.iter()).flatten().map(|byte| *byte).collect()
+    }
+
+    /// Return an iterator over the chunks of all elements following this one.
+    pub fn iter(&self) -> impl Iterator<Item = &[u8]> {
+        std::iter::successors(self.next.as_ref(), |element| element.0.next.as_ref())
+            .map(|(_, data)| data.as_ref())
+    }
+}
+
+/// A mutable space that dynamically allocates memory.
+/// 
+/// This space uses a linked list of [`SpaceElement`s](struct.SpaceElement.html) to allocate memory. Every time `allocate` is called, a new element is appended to the list and a new byte slice is created.
+/// 
+/// In order to use this space and retrieve the written data once it was written, you create a `SpaceElement` and create a new head with it. Then, you use the head like any other `MutSpace` and when you're done, you retrieve the written data by either calling [`to_vec`](struct.SpaceElement.html#method.to_vec) or [`iter`](struct.SpaceElement.html#iter).
+/// 
+/// # Usage example
+/// 
+/// ```
+/// # use lv2_core::prelude::*;
+/// # use lv2_urid::prelude::*;
+/// # use lv2_urid::mapper::*;
+/// # use lv2_atom::prelude::*;
+/// # use lv2_atom::space::*;
+/// # let mut mapper = Box::pin(HashURIDMapper::new());
+/// # let interface = mapper.as_mut().make_map_interface();
+/// # let map = Map::new(&interface);
+/// // URID cache creation is omitted.
+/// let urids: AtomURIDCache = map.populate_cache().unwrap();
+/// 
+/// // Creating the first element in the list and the writing head.
+/// let mut element = SpaceElement::default();
+/// let mut head = SpaceHead::new(&mut element);
+/// 
+/// // Writing an integer.
+/// let mut frame = (&mut head as &mut dyn MutSpace).create_atom_frame(urids.int).unwrap();
+/// Int::init(frame, 42).unwrap();
+/// 
+/// // Retrieving a continuos vector with the written data and verifying it's contents.
+/// let written_data: Vec<u8> = element.to_vec();
+/// let atom = UnidentifiedAtom::new(Space::from_slice(written_data.as_ref()));
+/// assert_eq!(42, atom.read(urids.int, ()).unwrap());
+/// ```
+pub struct SpaceHead<'a> {
+    element: Option<&'a mut SpaceElement>,
+    allocated_space: usize,
+}
+
+impl<'a> SpaceHead<'a> {
+    /// Create a new head that references the given element.
+    pub fn new(element: &'a mut SpaceElement) -> Self {
         Self {
-            blocks: Vec::new(),
-            allocated_bytes: 0,
+            element: Some(element),
+            allocated_space: 0,
         }
     }
+
+    fn internal_allocate(&mut self, size: usize) -> Option<&'a mut [u8]> {
+        let element = self.element.take()?;
+        let (new_element, new_space) = element.allocate(size)?;
+        self.element = Some(new_element);
+        self.allocated_space += size;
+        Some(new_space)
+    }
 }
 
-impl<'a> MutSpace<'a> for BufferedMutSpace {
+impl<'a> MutSpace<'a> for SpaceHead<'a> {
     fn allocate(&mut self, size: usize, apply_padding: bool) -> Option<(usize, &'a mut [u8])> {
-        let padding = if apply_padding {
-            8 - self.allocated_bytes % 8
+        let padding: usize = if apply_padding {
+            (8 - self.allocated_space % 8) % 8
         } else {
             0
-        } % 8;
+        };
 
         if padding != 0 {
-            self.blocks
-                .push(Pin::new(Box::from_iter((0..padding).map(|_| 0))));
+            self.internal_allocate(padding);
         }
 
-        self.blocks
-            .push(Pin::new(Box::from_iter((0..size).map(|_| 0))));
-        let data = self.blocks.last_mut().unwrap().as_mut().get_mut();
-        assert_eq!(size, data.len());
-
-        let data = unsafe { std::slice::from_raw_parts_mut(data as *mut _ as *mut u8, size) };
-        self.allocated_bytes += padding + size;
-        Some((padding, data))
+        self.internal_allocate(size)
+            .map(|new_space| (padding, new_space))
     }
 }
 
@@ -488,9 +555,10 @@ mod tests {
     }
 
     #[test]
-    fn test_buffered_mut_space() {
-        let space = BufferedMutSpace::default();
-        test_mut_space(space);
+    fn test_space_head() {
+        let mut space = SpaceElement::default();
+        let head = SpaceHead::new(&mut space);
+        test_mut_space(head);
     }
 
     #[test]
