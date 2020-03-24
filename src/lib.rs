d98a86174c62e933393ebf02cc98908dcfdde59b
Jan-Oliver 'Janonard' Opdenhövel <jan.opdenhoevel@protonmail.com>
2020-03-24 16:29:56 +0100

    Switching back to pub extern crates

--- src/lib.rs
+++ src/lib.rs
@@ -106,6 +106,10 @@
 //! * `minimal_plugin`: The bare minimum to create plugins. Includes `lv2-core` and `urid`.
 //! * `plugin`: Usual crates for standard plugins. Includes `lv2-core`, `lv2-atom`, `lv2-midi`, `lv2-urid`, and `urid`. **This is the default.**
 //! * `full`: All sub-crates.
+//! 
+//! # Extending
+//! 
+//! Please note that this re-export crate is only meant to be used by plugin projects. If you want to extend the framework with your own crates, please use the sub-crates as your dependencies instead. This will dramatically boost building durations and backwards compability.
 
 pub mod prelude {
     #[cfg(feature = "lv2-atom")]
@@ -129,44 +133,28 @@ pub mod prelude {
 }
 
 #[cfg(feature = "lv2-atom")]
-pub mod atom {
-    pub use lv2_atom::*;
-}
+pub extern crate lv2_atom;
 
 #[cfg(feature = "lv2-core")]
-pub mod core {
-    pub use lv2_core::*;
-}
+pub extern crate lv2_core;
 
 #[cfg(feature = "lv2-midi")]
-pub mod midi {
-    pub use lv2_midi::*;
-}
+pub extern crate lv2_midi;
 
 #[cfg(feature = "lv2-state")]
-pub mod state {
-    pub use lv2_state::*;
-}
+pub extern crate lv2_state;
 
 #[cfg(feature = "lv2-time")]
-pub mod time {
-    pub use lv2_time::*;
-}
+pub extern crate lv2_time;
+
+#[cfg(feature = "urid")]
+pub extern crate urid;
 
 #[cfg(feature = "lv2-units")]
-pub mod units {
-    pub use lv2_units::*;
-}
+pub extern crate lv2_units;
 
-#[cfg(any(feature = "lv2-urid", feature = "urid"))]
-pub mod urid {
-    #[cfg(feature = "lv2-urid")]
-    pub use lv2_urid::*;
-    #[cfg(feature = "urid")]
-    pub use urid::*;
-}
+#[cfg(feature = "lv2-urid")]
+pub extern crate lv2_urid;
 
 #[cfg(feature = "lv2-worker")]
-pub mod worker {
-    pub use lv2_worker::*;
-}
+pub extern crate lv2_worker;
