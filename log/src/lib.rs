101d936b645d36660da22b330b08b2e5abba2fef
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-03-26 01:58:06 +0100

    impl Uribound and LogURIDcollection for type of log

--- log/src/lib.rs
+++ log/src/lib.rs
@@ -2,6 +2,31 @@ use lv2_core::feature::*;
 use std::os::raw::*; //get all common c_type
 use urid::*;
 
+pub struct EntryClass;
+unsafe impl UriBound for EntryClass {
+    const URI: &'static [u8] = lv2_sys::LV2_LOG__Entry;
+}
+
+pub struct ErrorClass;
+unsafe impl UriBound for ErrorClass {
+    const URI: &'static [u8] = lv2_sys::LV2_LOG__Error;
+}
+
+pub struct NoteClass;
+unsafe impl UriBound for NoteClass {
+    const URI: &'static [u8] = lv2_sys::LV2_LOG__Note;
+}
+
+pub struct TraceClass;
+unsafe impl UriBound for TraceClass {
+    const URI: &'static [u8] = lv2_sys::LV2_LOG__Trace;
+}
+
+pub struct WarningClass;
+unsafe impl UriBound for WarningClass {
+    const URI: &'static [u8] = lv2_sys::LV2_LOG__Warning;
+}
+
 /// The Log feature
 #[repr(transparent)]
 pub struct Log<'a> {
@@ -9,7 +34,7 @@ pub struct Log<'a> {
 }
 
 unsafe impl<'a> UriBound for Log<'a> {
-    const URI: &'static [u8] = lv2_sys::LV2_WORKER__schedule;
+    const URI: &'static [u8] = lv2_sys::LV2_LOG__log;
 }
 
 unsafe impl<'a> Feature for Log<'a> {
@@ -20,6 +45,17 @@ unsafe impl<'a> Feature for Log<'a> {
             .as_ref()
             .map(|internal| Self { internal })
     }
+}
+
+/// A URID cache containing all time properties.
+#[derive(URIDCollection)]
+pub struct LogURIDCollection {
+    pub entry_class: URID<EntryClass>,
+    pub error_class: URID<ErrorClass>,
+    pub note_class: URID<NoteClass>,
+    pub trace_class: URID<TraceClass>,
+    pub warning_class: URID<WarningClass>,
+    //pub log: URID<Log<'a>>,
 }
 
 #[cfg(test)]
