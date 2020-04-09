d4888a00987ea6fc1cce2e711d938a8f30c36b12
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-04-10 01:38:54 +0200

    break prebuild step if bindings not compatible with target
    An error message instruct to use bindgen feature

--- sys/build.rs
+++ sys/build.rs
@@ -1,15 +1,29 @@
 #[cfg(not(feature = "bindgen"))]
-fn main() {
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
     use std::env;
     use std::fs;
     use std::path::PathBuf;
 
-    let mut source_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-    source_path.push("build_data");
-    source_path.push("bindings.rs");
+    let mut data_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    data_path.push("build_data");
+
+    let target = env::var("TARGET").unwrap();
+    let valid_targets = fs::read_to_string(data_path.join("valid_targets.txt"))
+        .expect("can't find \"valid_targets.txt\"");
+    if !valid_targets.contains(&target) {
+        let s = format!("No valid prebinding for {}. ", target)
+            + "Add \"lv2_sys\" with \"bindgen\" feature in your dependencies.";
+        eprintln!("{}", s);
+        std::process::exit(-1);
+        //return Err("No prebinding for the target".into()) ;
+    }
+
     let mut out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     out_path.push("bindings.rs");
-    fs::copy(source_path, out_path).unwrap();
+    fs::copy(data_path.join("bindings.rs"), out_path).unwrap();
+    Ok(())
 }
 
 #[cfg(feature = "bindgen")]
