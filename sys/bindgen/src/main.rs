594f00d1db2ea833afac66d120cc1d17f1be78e5
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-04-11 01:41:46 +0200

    function that get the 'layout' type used for a C enum

--- sys/bindgen/src/main.rs
+++ sys/bindgen/src/main.rs
@@ -1,9 +1,12 @@
 extern crate bindgen;
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::io::Write;
 use std::path::PathBuf;
 use std::process::Command;
+
+type DynError = Box<dyn Error>;
 
 fn main() {
     generate_bindings();
@@ -77,5 +80,39 @@ fn generate_valid_target() {
     let mut f = fs::File::create(out_path).unwrap();
     for target in valid_targets {
         writeln!(f, "{}", target).unwrap();
+    }
+}
+
+/// Return the target enum representation or error if bindgen panics
+fn get_target_enum(target: &str) -> Result<String, DynError> {
+    let mut test_h = PathBuf::new();
+    test_h.push(env::var("CARGO_MANIFEST_DIR")?);
+    test_h.push("enum_test.h");
+    let test_h = test_h.to_str().unwrap();
+    let test_h = String::from(test_h);
+    let target = String::from(target);
+    //the thread spawning avoid to exit when bindgen panics
+    let handle = thread::spawn(move || {
+        let builder = bindgen::Builder::default()
+            .size_t_is_usize(true)
+            .clang_arg(format!("--target={}", target))
+            .header(test_h);
+        // make silent panic
+        std::panic::set_hook(Box::new(|_| ()));
+        let bindings = builder.generate().unwrap();
+        //restore default panic hook because it's not thread local
+        let _ = std::panic::take_hook();
+        bindings.to_string()
+    });
+    let res = handle.join();
+    match res {
+        Ok(res) => {
+            let pat = "pub type test = ";
+            let i1 = res.find(pat).unwrap() + pat.len();
+            let i2 = i1 + res.get(i1..).unwrap().find(';').unwrap();
+            let repr = res.get(i1..i2).unwrap();
+            Ok(String::from(repr))
+        }
+        Err(_) => Err("bindgen panicked".into()),
     }
 }
