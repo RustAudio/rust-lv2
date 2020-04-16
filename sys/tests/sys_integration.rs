58c40a0205efafd1ac67b743ddf7033f149eda81
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-04-16 18:50:03 +0200

    use syn and hashset to check bindings equivalence.
    the old method didn't work because generated bindings have different
    formatting and declaration order accross plateform

--- sys/tests/sys_integration.rs
+++ sys/tests/sys_integration.rs
@@ -1,47 +1,92 @@
+extern crate lv2_sys_bindgen;
+use quote::quote;
+use proc_macro2::Span;
+use std::collections::HashSet;
+use std::fs;
+use std::path::PathBuf;
+use syn::{Field, Fields, FieldsUnnamed, Ident, Item, ItemStruct, ItemType, Path, Type, TypePath};
+
+use std::env;
+
+// this function allow to ignore some i32/u32 difference
+fn i32_to_u32(mut item: Item) -> Item {
+    match &mut item {
+        Item::Type(ItemType { ty, .. }) => {
+            if let Type::Path(TypePath {
+                path: Path { segments, .. },
+                ..
+            }) = ty.as_mut()
+            {
+                for e in segments.iter_mut() {
+                    if format!("{}", e.ident) == "i32" {
+                        e.ident = Ident::new("u32", Span::call_site());
+                    }
+                }
+            }
+        }
+        Item::Struct(ItemStruct {
+            fields: Fields::Unnamed(FieldsUnnamed { unnamed, .. }),
+            ..
+        }) => {
+            for u in unnamed {
+                if let Field {
+                    ty:
+                        Type::Path(TypePath {
+                            path: Path { segments, .. },
+                            ..
+                        }),
+                    ..
+                } = u
+                {
+                    for e in segments.iter_mut() {
+                        if format!("{}", e.ident) == "i32" {
+                            e.ident = Ident::new("u32", Span::call_site());
+                        }
+                    }
+                }
+            }
+        }
+        _ => (),
+    }
+    item
+}
+
+// the idea here is to represent a binding file as a unordonned collection of syn::Item. This to be
+// insensitive to formatting or definition order in the file.
 #[test]
 fn bindings_are_equivalent() {
-    extern crate lv2_sys_bindgen;
-    use std::env;
-    use std::fs::File;
-    use std::io::BufRead;
-    use std::io::BufReader;
-    use std::iter::Iterator;
-    use std::path::PathBuf;
 
     let work_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
     let source_dir = work_dir.join("lv2");
     let bindings1_dir = work_dir.join("build_data");
     let bindings2_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    println!("{}", bindings2_dir.to_str().unwrap());
+    //println!("{}", bindings2_dir.to_str().unwrap());
     lv2_sys_bindgen::generate_bindings(&source_dir, &bindings2_dir, None);
+    
+    let f1 = fs::read_to_string(bindings1_dir.join("bindings.rs")).unwrap();
+    let f1 = syn::parse_str::<syn::File>(&f1).unwrap();
+    let h1: HashSet<_> = f1.items.into_iter().map(i32_to_u32).collect();
 
-    let f1 = File::open(bindings1_dir.join("bindings.rs")).unwrap();
-    let mut f1 = BufReader::new(f1).lines();
+    let f2 = fs::read_to_string(bindings2_dir.join("bindings.rs")).unwrap();
+    let f2 = syn::parse_str::<syn::File>(&f2).unwrap();
+    let h2: HashSet<_> = f2.items.into_iter().map(i32_to_u32).collect();
 
-    let f2 = File::open(bindings2_dir.join("bindings.rs")).unwrap();
-    let mut f2 = BufReader::new(f2).lines();
-
-    let mut line_count = 1_usize;
-
-    loop {
-        let l1 = f1.next();
-        let l2 = f2.next();
-        match (l1, l2) {
-            (Some(l1), Some(l2)) => {
-                let l1 = l1.unwrap().replace("i32", "u32");
-                let l2 = l2.unwrap().replace("i32", "u32");
-                if l1 != l2 {
-                    panic!(
-                        "line {}: bindings aren't equivalent\npre-bindings: {}\ngenerated   : {}",
-                        line_count, l1, l2
-                    );
-                }
+    if h1 != h2 {
+        let diff1: HashSet<_> = h1.difference(&h2).collect();
+        let diff2: HashSet<_> = h2.difference(&h1).collect();
+        let mut message = String::from("Error, binding aren't equivalent\n");
+        if !diff1.is_empty() {
+            message.push_str("Item present only in static bindings:\n");
+            for e in diff1 {
+                message.push_str(&format!("{}\n", quote!(#e)));
             }
-            (Some(_), None) => panic!("pre-bindings contains more lines than generated bindings"),
-            (None, Some(_)) => panic!("generated bindings contains more lines than pre-bindings"),
-            (None, None) => break,
         }
-        //counting line
-        line_count += 1;
+        if !diff2.is_empty() {
+            message.push_str("Item present only in generated bindings:\n");
+            for e in diff2 {
+                message.push_str(&format!("{}\n", quote!(#e)));
+            }
+        }
+        panic!(message);
     }
 }
