5fd9db00fd16be5f85ea6ec56136547b1fc207e5
Jan-Oliver 'Janonard' Opdenhövel <jan.opdenhoevel@protonmail.com>
2020-05-16 12:40:31 +0200

    Extending the installation script for targets

--- install_examples.sh
+++ install_examples.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env sh
 set -e -x
+[[ ! -z $TARGET ]] || (export TARGET="x86_64-unknown-linux-gnu")
 
 rm -rf target/lv2
 mkdir -p target/lv2
 
-cargo build -p amp --release
+cargo build -p amp --release --target $TARGET
 cp -r docs/amp/eg-amp-rs.lv2 target/lv2/eg-amp-rs.lv2
-cp target/release/libamp.so target/lv2/eg-amp-rs.lv2
+cp target/$TARGET/release/libamp.so target/lv2/eg-amp-rs.lv2
 
-cargo build -p midigate --release
+cargo build -p midigate --release --target $TARGET
 cp -r docs/midigate/eg-midigate-rs.lv2 target/lv2/eg-midigate-rs.lv2
-cp target/release/libmidigate.so target/lv2/eg-midigate-rs.lv2
+cp target/$TARGET/release/libmidigate.so target/lv2/eg-midigate-rs.lv2
 
-cargo build -p fifths --release
+cargo build -p fifths --release --target $TARGET
 cp -r docs/fifths/eg-fifths-rs.lv2 target/lv2/eg-fifths-rs.lv2
-cp target/release/libfifths.so target/lv2/eg-fifths-rs.lv2
+cp target/$TARGET/release/libfifths.so target/lv2/eg-fifths-rs.lv2
 
-cargo build -p metro --release
+cargo build -p metro --release --target $TARGET
 cp -r docs/metro/eg-metro-rs.lv2 target/lv2/eg-metro-rs.lv2
-cp target/release/libmetro.so target/lv2/eg-metro-rs.lv2
+cp target/$TARGET/release/libmetro.so target/lv2/eg-metro-rs.lv2
 
 mkdir -p ~/.lv2
 cp -r target/lv2/* ~/.lv2
