b02e735293b2922b28d3d2f83f83d4d07c34f86e
Johannes Mueller <github@johannes-mueller.org>
2020-06-20 13:30:27 +0200

    Apply rust format to urid/lv2_urid/src/lib.rs

--- urid/lv2-urid/src/lib.rs
+++ urid/lv2-urid/src/lib.rs
@@ -4,9 +4,9 @@
 //!
 //! Since this crate depends on `-sys` crates that use `bindgen` to create the C API bindings,
 //! you need to have clang installed on your machine.
+extern crate lv2_atom as atom;
 extern crate lv2_core as core;
 extern crate lv2_sys as sys;
-extern crate lv2_atom as atom;
 
 mod feature;
 mod mapper;
