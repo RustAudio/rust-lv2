9cc776cefc53e921692e2a98b6c4cd2a7d2f1208
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-07-18 19:28:16 +0200

    add experimental bindings for arm and aarch64

--- sys/src/linux/mod.rs
+++ sys/src/linux/mod.rs
@@ -1,4 +1,13 @@
 #[cfg_attr(target_arch = "x86", path = "x86.rs")]
 #[cfg_attr(target_arch = "x86_64", path = "x86_64.rs")]
-mod unsupported;
-pub use unsupported::*;
+//bindings are identical between soft float arm, hard float arm and aarch64
+#[cfg_attr(
+    all(
+        target_arch = "arm",
+        target_arch = "aarch64",
+        feature = "experimental-targets"
+    ),
+    path = "arm.rs"
+)]
+mod toto;
+pub use toto::*;
