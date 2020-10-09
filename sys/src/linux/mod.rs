152fe5c62227128b66ad41f5901c838b763552f6
Yruama_Lairba <yruama_lairba@hotmail.com>
2020-10-09 23:49:25 +0200

    separate aarch64 and arm binding, fix a crap

--- sys/src/linux/mod.rs
+++ sys/src/linux/mod.rs
@@ -1,13 +1,6 @@
 #[cfg_attr(target_arch = "x86", path = "x86.rs")]
 #[cfg_attr(target_arch = "x86_64", path = "x86_64.rs")]
-//bindings are identical between soft float arm, hard float arm and aarch64
-#[cfg_attr(
-    all(
-        target_arch = "arm",
-        target_arch = "aarch64",
-        feature = "experimental-targets"
-    ),
-    path = "arm.rs"
-)]
-mod toto;
-pub use toto::*;
+#[cfg_attr(target_arch = "arm", path = "arm.rs")]
+#[cfg_attr(target_arch = "aarch64", path = "aarch64.rs")]
+mod unsupported;
+pub use unsupported::*;
