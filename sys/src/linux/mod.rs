31d72e21fa7dfad15cc2d5a1f2e2541aa4a12829
taylor.fish <contact@taylor.fish>
2024-02-23 17:40:24 -0800

    Add support for 64-bit PowerPC

--- sys/src/linux/mod.rs
+++ sys/src/linux/mod.rs
@@ -2,5 +2,6 @@
 #[cfg_attr(target_arch = "x86_64", path = "x86_64.rs")]
 #[cfg_attr(target_arch = "arm", path = "arm.rs")]
 #[cfg_attr(target_arch = "aarch64", path = "aarch64.rs")]
+#[cfg_attr(target_arch = "powerpc64", path = "powerpc64.rs")]
 mod unsupported;
 pub use unsupported::*;
